f206331c7e3c95b8154f2cfd002d6b9367f8d1b4
Luan Pham <phamquiluan@gmail.com>
2020-08-05 14:06:15 +0700

    Add automatically download checkpoints

--- resattnet/resattnet56.py
+++ resattnet/resattnet56.py
@@ -1,13 +1,16 @@
+import os
+import torch
 import torch.nn as nn
 from pytorchcv.model_provider import get_model as ptcv_get_model
 
 
-def resattnet56(in_channels, num_classes, pretrained=True):
+def resattnet56(in_channels=3, num_classes=1000, pretrained=True):
     model = ptcv_get_model("resattnet56", pretrained=False)
 
     if pretrained is True:
-        # load pretrained automatically
-        pass
-
+        state = torch.hub.load_state_dict_from_url(
+            "https://github.com/phamquiluan/ResidualAttentionNetwork/releases/download/v0.1.0/resattnet56.pth"
+        )
+        model.load_state_dict(state["state_dict"])
     model.output = nn.Linear(2048, num_classes)
     return model
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 from setuptools import find_packages, setup
 
-version = "0.0.1"
+version = "0.1.0"
 cwd = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -29,6 +29,6 @@ setup(
         "imgaug",
         "tensorboard",
         "sklearn",
-        "gputil"
+        "gputil",
     ],
 )
